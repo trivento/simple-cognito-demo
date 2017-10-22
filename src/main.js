fc5d9da1222bd1a8defdfdea5cf7e8b1a00b37d2
Ruud van Vliet <rimvanvliet@gmail.com>
2017-10-22 17:14:28 +0200

    added missing configuration alert

--- src/main.js
+++ src/main.js
@@ -7,33 +7,37 @@ import ConfirmRegistrationForm from "./confirmRegistration.js";
 import ForgotPasswordForm from "./forgotPassword.js";
 import SignInForm from "./signin.js";
 
-
 import appConfig from "./config";
 
-Config.region = appConfig.region;
-Config.credentials = new CognitoIdentityCredentials({
-  IdentityPoolId: appConfig.IdentityPoolId
-});
 
-const userPool = new CognitoUserPool({
-  UserPoolId: appConfig.UserPoolId,
-  ClientId: appConfig.ClientId,
-});
+if (!appConfig.region || !appConfig.UserPoolId || !appConfig.ClientId) {
+  alert("Configuration is missing ...\nPlease edit the configuration file in ./src/config.js\nto configure your AWS User pool.")
+} else {
+  Config.region = appConfig.region;
+  Config.credentials = new CognitoIdentityCredentials({
+    IdentityPoolId: appConfig.IdentityPoolId
+  });
 
-const signupDiv = document.createElement('div');
-signupDiv.setAttribute("id", "signup");
-document.body.appendChild(signupDiv);
-const confirmRegistrationDiv = document.createElement('div');
-confirmRegistrationDiv.setAttribute("id", "confirmRegistration");
-document.body.appendChild(confirmRegistrationDiv);
-const forgotPasswordDiv = document.createElement('div');
-forgotPasswordDiv.setAttribute("id", "forgotPassword");
-document.body.appendChild(forgotPasswordDiv);
-const signinDiv = document.createElement('div');
-signinDiv.setAttribute("id", "signin");
-document.body.appendChild(signinDiv);
+  const userPool = new CognitoUserPool({
+    UserPoolId: appConfig.UserPoolId,
+    ClientId: appConfig.ClientId,
+  });
 
-ReactDOM.render(<SignUpForm userPool={userPool}/>, document.getElementById('signup'));
-ReactDOM.render(<ConfirmRegistrationForm userPool={userPool}/>, document.getElementById('confirmRegistration'));
-ReactDOM.render(<ForgotPasswordForm userPool={userPool}/>, document.getElementById('forgotPassword'));
-ReactDOM.render(<SignInForm userPool={userPool}/>, document.getElementById('signin'));
+  const signupDiv = document.createElement('div');
+  signupDiv.setAttribute("id", "signup");
+  document.body.appendChild(signupDiv);
+  const confirmRegistrationDiv = document.createElement('div');
+  confirmRegistrationDiv.setAttribute("id", "confirmRegistration");
+  document.body.appendChild(confirmRegistrationDiv);
+  const forgotPasswordDiv = document.createElement('div');
+  forgotPasswordDiv.setAttribute("id", "forgotPassword");
+  document.body.appendChild(forgotPasswordDiv);
+  const signinDiv = document.createElement('div');
+  signinDiv.setAttribute("id", "signin");
+  document.body.appendChild(signinDiv);
+
+  ReactDOM.render(<SignUpForm userPool={userPool}/>, document.getElementById('signup'));
+  ReactDOM.render(<ConfirmRegistrationForm userPool={userPool}/>, document.getElementById('confirmRegistration'));
+  ReactDOM.render(<ForgotPasswordForm userPool={userPool}/>, document.getElementById('forgotPassword'));
+  ReactDOM.render(<SignInForm userPool={userPool}/>, document.getElementById('signin'));
+}
